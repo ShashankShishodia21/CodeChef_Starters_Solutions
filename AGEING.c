99e874eb6a8628c1b88f0204e42344b147ccbf6e
Shashank Shishodia <115582986+ShashankShishodia21@users.noreply.github.com>
2023-07-12 19:18:14 +0530

    Create AGEING.c

---
+++ AGEING.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+		int t;
+	scanf("%d",&t);
+	while(t--){
+	    int x,y,z;
+	    scanf("%d",&x);
+	    printf("%d\n",x-10);
+	    
+	}
+
+	return 0;
+}
+
