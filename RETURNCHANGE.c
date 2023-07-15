87a1d61b7dbac24706eee2ebe2fe95235d9d00fb
Shashank Shishodia <115582986+ShashankShishodia21@users.noreply.github.com>
2023-07-15 23:01:27 +0530

    Create RETURNCHANGE.c

---
+++ RETURNCHANGE.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--){
+	    int x;
+	    scanf("%d",&x);
+	    if(x%10 < 5){
+	        x = x - (x%10);
+	         printf("%d\n",100-x);
+	    }
+	    else if(x%10 >= 5){
+	        x = x - (x%10);
+	        x = x+10;
+	        printf("%d\n",100-x);
+	    }
+	}
+	return 0;
+}
+
