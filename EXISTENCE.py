0a349dafbe9dd57561100146e695b781d0a72ff5
Shashank Shishodia <115582986+ShashankShishodia21@users.noreply.github.com>
2023-07-16 21:05:17 +0530

    Create EXISTENCE.py

---
+++ EXISTENCE.py
@@ -0,0 +1,13 @@
+# cook your dish here
+# cook your code here
+import sys
+t = int(input())
+for i in range(t):
+    x, y = map(int, input().split())
+    if((x**4) + 4 * (y**2) == 4 * (x**2) * y):
+        print("yes")
+        
+    else:
+        print("no")
+            
+        
