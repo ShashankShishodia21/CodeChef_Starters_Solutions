aae9ae9d8f6c3e8020ac1c7c502e13b53556de43
Shashank Shishodia <115582986+ShashankShishodia21@users.noreply.github.com>
2023-07-17 22:37:51 +0530

    Create DDMMORMMDD.py

---
+++ DDMMORMMDD.py
@@ -0,0 +1,22 @@
+def identify_date_format(date_str):
+    day, month, year = map(int, date_str.split('/'))
+    if day > 12:
+        return "DD/MM/YYYY"
+    elif month > 12:
+        return "MM/DD/YYYY"
+    else:
+        return "BOTH"
+
+# input the number of test cases
+t = int(input())
+
+# loop over the test cases
+for i in range(t):
+    # input the date string
+    date_str = input()
+    
+    # identify the date format
+    date_format = identify_date_format(date_str)
+    
+    # print the result
+    print(date_format)
