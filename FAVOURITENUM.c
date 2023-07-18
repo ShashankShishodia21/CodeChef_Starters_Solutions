f725763a85c3e4bd3917768718e97fa5a9af5c1d
Shashank Shishodia <115582986+ShashankShishodia21@users.noreply.github.com>
2023-07-18 22:42:13 +0530

    Create FAVOURITENUM.c

---
+++ FAVOURITENUM.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--){
+	    int a;
+	    scanf("%d",&a);
+	    if(a%2==0 && a%7==0){
+	        printf("Alice\n");
+	    }
+	    else if(a%2!=0 && a%9==0){
+	        printf("Bob\n");
+	    }
+	    else{
+	        printf("Charlie\n");
+	    }
+	}
+	return 0;
+}
+
