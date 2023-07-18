3f77575083edb0c554dc5331b7d953b860221940
Shashank Shishodia <115582986+ShashankShishodia21@users.noreply.github.com>
2023-07-18 22:44:51 +0530

    Create SEATNUMBER.c

---
+++ SEATNUMBER.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--){
+	int x;
+	scanf("%d",&x);
+	if(x>=1 && x<=10){
+	    printf("Lower Double\n");
+	}
+	else if(x>=11 && x<=15){
+	    printf("Lower Single\n");
+	}
+	else if(x>=16 && x<=25){
+	    printf("Upper Double\n");
+	}
+	else if(x>=26 && x<=30){
+	    printf("Upper Single\n");
+	}
+	}
+	return 0;
+}
+
