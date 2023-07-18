742a2e400e673de20172ff91f4842ec53fee12fb
Shashank Shishodia <115582986+ShashankShishodia21@users.noreply.github.com>
2023-07-18 22:41:10 +0530

    Create LEARNSQL.c

---
+++ LEARNSQL.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	    int a,b,c;
+	    scanf("%d %d %d",&a,&b,&c);
+	    int sum = a+c;
+	    printf("%d\n",sum*b);
+	
+	return 0;
+}
+
