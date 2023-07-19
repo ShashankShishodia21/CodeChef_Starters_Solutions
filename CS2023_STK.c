fd7640c8aae18893feff0392ed6aa85e3e6a6a31
Shashank Shishodia <115582986+ShashankShishodia21@users.noreply.github.com>
2023-07-19 20:13:36 +0530

    Update CS2023_STK.c

--- CS2023_STK.c
+++ CS2023_STK.c
@@ -2,44 +2,44 @@
 
 int main(void) {
     int T;
-    scanf("%d", &T); // Read the number of test cases
+    scanf("%d", &T);
 
     while (T--) {
         int N;
-        scanf("%d", &N); // Read the number of days
+        scanf("%d", &N);
 
         int om[N], addy[N];
         for (int i = 0; i < N; i++) {
-            scanf("%d", &om[i]); // Read Om's problem count for each day
+            scanf("%d", &om[i]);
         }
         for (int i = 0; i < N; i++) {
-            scanf("%d", &addy[i]); // Read Addy's problem count for each day
+            scanf("%d", &addy[i]); 
         }
 
         int om_streak = 0, addy_streak = 0;
         int om_max_streak = 0, addy_max_streak = 0;
 
         for (int i = 0; i < N; i++) {
-            // Update Om's streak
+           
             if (om[i] > 0) {
                 om_streak++;
             } else {
                 om_streak = 0;
             }
             
-            // Update Addy's streak
+            
             if (addy[i] > 0) {
                 addy_streak++;
             } else {
                 addy_streak = 0;
             }
 
-            // Update Om's maximum streak
+            
             if (om_streak > om_max_streak) {
                 om_max_streak = om_streak;
             }
 
-            // Update Addy's maximum streak
+           
             if (addy_streak > addy_max_streak) {
                 addy_max_streak = addy_streak;
             }
