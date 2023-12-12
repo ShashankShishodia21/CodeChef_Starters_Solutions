2001402e6a1089d3c676d51668ae6b0663efc16a
Shashank Shishodia <115582986+ShashankShishodia21@users.noreply.github.com>
2023-12-12 20:15:44 +0530

    Update EPANLINK.py

--- EPANLINK.py
+++ EPANLINK.py
@@ -6,16 +6,10 @@ num_test_cases = int(input())
 for i in range(num_test_cases):
     
 
-    # Input value of N
     N = int(input())
 
-    # Number of officers in Chefland
     officers = 20
-
-    # Number of applications each officer would process
     x = N // officers
-
-    # Number of unprocessed applications
     unprocessed = N % officers
 
     print(unprocessed)
