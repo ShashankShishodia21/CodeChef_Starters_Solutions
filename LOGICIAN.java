833a036afee5e898e2343bf811620c1ac9d477ac
Shashank Shishodia <115582986+ShashankShishodia21@users.noreply.github.com>
2024-01-12 22:24:00 +0530

    Update LOGICIAN.java

--- LOGICIAN.java
+++ LOGICIAN.java
@@ -4,7 +4,6 @@ import java.io.*;
 class Codechef {
 public static void main (String[] args) throws java.lang.Exception
 {
-// your code goes here
     Scanner sc = new Scanner(System.in);
     int t=sc.nextInt();
     
