c3792e1b755f2b1167604154291d4dcfb188e5f1
Shashank Shishodia <115582986+ShashankShishodia21@users.noreply.github.com>
2024-01-14 00:11:18 +0530

    Create ChrisDecor.c

---
+++ ChrisDecor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--){
+	    int n,x,y;
+	    scanf("%d %d %d",&n,&x,&y);
+	    int remy = y/3;
+	    int remx = x - remy;
+	    int remn = n - remy;
+	    if((x>=n && y>=n*3) || (x>=n*2)){
+	        printf("YES\n");
+	    }
+	    else if((remx >= remn) && (remn <= remx/2)){
+	        printf("YES\n");
+	    }
+	    else{
+	        printf("NO\n");
+	    }
+	}
+
+}
+
